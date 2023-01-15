1c22e61d069237c46429367d437f4bc32279f552
donkeyrat <darklordbananas@gmail.com>
2023-01-14 23:17:10 -0800

    alternating shoot positions

---
+++ AlternateShootPosition.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Holiday
+{
+    public class AlternateShootPosition : MonoBehaviour
+    {
+        public void Switch()
+        {
+            shootPosition.transform.position = positions[index];
+            index++;
+        }
+
+        private int index;
+
+        public Transform shootPosition;
+
+        public List<Vector3> positions;
+    }
+}
